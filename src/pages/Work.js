626231c6502bc9bc18ab3460c1f0b185b2de771d
KAW <36702039+kerry91@users.noreply.github.com>
2022-11-18 05:41:41 +0000

    .

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -3,7 +3,7 @@ import "./Pages.css";
 const Work = () => {
   return (
     <>
-      <section class="text-gray-700 body-font h-screen">
+      <section class="text-gray-700 body-font">
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
