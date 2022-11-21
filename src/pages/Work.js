e26565e16fb3ba12a06701fc8fa9cad50ebf6aaa
KAW <36702039+kerry91@users.noreply.github.com>
2022-11-21 05:15:51 +0000

    .

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -166,13 +166,38 @@ const Work = () => {
                 </a>
               </div>
             </div>
-                        <div class="xl:w-1/3 md:w-1/2 p-4">
+            <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-300 p-6 rounded-lg">
                   <Wcard 
                   wCardtitle="Advice Generator"
                   wCarddescription="This is a Frontend Mentor project. It is made up of React, Bulma CSS and uses the Advice Slip API" />
                 <a
                   href="https://kaw-advice-generator.netlify.app"
+                  class="mt-3 text-indigo-500 inline-flex items-center"
+                >
+                  Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+              <div class="border border-gray-300 p-6 rounded-lg">
+                  <Wcard 
+                  wCardtitle="Pokedex"
+                  wCarddescription="The Pokedex is made up from React, CSS and the PokeAPI" />
+                  <br/>
+                <a
+                  href="https://kaw-pokedex.netlify.app"
                   class="mt-3 text-indigo-500 inline-flex items-center"
                 >
                   Learn More
