3216d82cdbe0644bf0fa14eb82ccc83d93683890
kerry91 <kerry91warnes@gmail.com>
2022-12-17 18:36:05 +0000

    .

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,10 +2,11 @@ import './Contact.scss'
 export default function Contact () {
     return(
         <div className='Contact'>
-            <h1 class="sm:text-3xl text-2xl font-medium title-font text-center">
+            <h1 class="sm:text-2xl text-2xl font-medium title-font text-center">
               Contact Me
             </h1>
-            <h1 className='text-center text-7xl'>COMMIING SOON</h1>
+            <br/>
+            <p class="text-center"><button><a href="mailto:kerry91warnes@gmail.com" target="_blank" rel="noreferrer">Send me an email</a></button></p>
         </div>
     )
 }
