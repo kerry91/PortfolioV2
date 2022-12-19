846d8ccc38e9dc5e1c71bd3e17172b9fbc0c3a2d
KAW <36702039+kerry91@users.noreply.github.com>
2022-12-19 08:59:25 +0000

    .

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -11,12 +11,39 @@ export default function Work() {
               My Work
             </h1>
           </div>
-          <div class="flex flex-wrap -m-4">
+          <div class="flex flex-wrap">
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
+                <WorkCard
+                  WorkCardtitle="CV Builder"
+                  WorkCarddescription="This project is made up of React, Redux and Material-UI"
+                />
+                <a
+                  href="https://kaw-cv-builder.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="mt-3 text-indigo-500 inline-flex items-center"
+                >
+                  Visit
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Interactive Card Details"
-                  WorkCarddescription="This project is made up of HTML React.js, Redux and Bootstrap.css"
+                  WorkCarddescription="This project is made up of React, Redux and Bootstrap"
                 />
                 <a
                   href="https://kaw-interactive-card-details.netlify.app/"
@@ -40,14 +67,12 @@ export default function Work() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Interactive Rating"
                   WorkCarddescription=" This project is made up of HTML, CSS
                   and JavaScript"
                 />
-                <br />
-
                 <a
                   href="https://kaw-interactive-rating.netlify.app/"
                   target="_blank"
@@ -70,11 +95,11 @@ export default function Work() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Intro component with Signup Form"
                   WorkCarddescription="This project is made up of HTML,
-                  Bootstrap CSS and JavaScript"
+                  Bootstrap and JavaScript"
                 />
                 <a
                   href="https://kaw-intro-component.netlify.app/"
@@ -98,10 +123,10 @@ export default function Work() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Countries"
-                  WorkCarddescription="This project is made up of HTML, React.js and Tailwind.css and the REST Countries API"
+                  WorkCarddescription="This project is made up of React, Tailwind and the Countries API"
                 />
                 <a
                   href="https://kaw-countries.netlify.app"
@@ -125,10 +150,10 @@ export default function Work() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Advice Generator"
-                  WorkCarddescription="This is a Frontend Mentor project. It is made up of HTML React.js, Bulma.css and uses the Advice Slip API"
+                  WorkCarddescription="This is a Frontend Mentor project. It is made up of React, Bulma and the Advice Slip API"
                 />
                 <a
                   href="https://kaw-advice-generator.netlify.app"
@@ -152,10 +177,10 @@ export default function Work() {
               </div>
             </div>
             <div class="xl:w-1/3 md:w-1/2 sm:w-full p-4">
-              <div class="border border-gray-300 p-6 rounded-lg">
+              <div class="border border-gray-300 p-6 rounded-lg h-48">
                 <WorkCard
                   WorkCardtitle="Pokedex"
-                  WorkCarddescription="The Pokedex is made up from HTML React.js, CSS and the PokeAPI"
+                  WorkCarddescription="The Pokedex is made up from React, CSS and the PokeAPI"
                 />
                 <a
                   href="https://kaw-pokedex.netlify.app"
